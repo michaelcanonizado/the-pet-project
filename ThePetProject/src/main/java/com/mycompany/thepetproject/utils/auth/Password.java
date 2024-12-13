9d44339aa6721fd1b5306fcac6573333abfaacfc
unknown <michaelxaviercanonizado@gmail.com>
2024-12-13 15:53:59 +0800

    feat(Password): initialize class and add hash() method

---
+++ ThePetProject/src/main/java/com/mycompany/thepetproject/utils/auth/Password.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.thepetproject.utils.auth;
+
+/**
+ *
+ * @author lilac
+ */
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Password {
+    public static String hash(String password) {
+        try {
+            // Get an instance of SHA-256 algorithm
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            // Hash the password
+            byte[] hashedBytes = digest.digest(password.getBytes());
+            // Convert the hash into a Base64 string (or hexadecimal)
+            return Base64.getEncoder().encodeToString(hashedBytes);
+        } catch (NoSuchAlgorithmException err) {
+            throw new RuntimeException("Error hashing password", err);
+        }
+    }
+}
