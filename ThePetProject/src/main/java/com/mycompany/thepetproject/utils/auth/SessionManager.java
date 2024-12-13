16474238fa2baacf08a60ed551bae1f951102598
unknown <michaelxaviercanonizado@gmail.com>
2024-12-13 15:04:51 +0800

    feat(SessionManager): initialize singleton SessionManager and authenticate method

---
+++ ThePetProject/src/main/java/com/mycompany/thepetproject/utils/auth/SessionManager.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.thepetproject.utils.auth;
+
+/**
+ *
+ * @author lilac
+ */
+public class SessionManager {
+    private static SessionManager instance;
+    private boolean isLoggedIn = false;
+
+    private SessionManager() {};
+    
+    // Get the singleton instance
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+    
+    public boolean authenticate(String username, String password, AuthenticationStrategy strategy) {
+        if (strategy.authenticate(username, password)) {
+            isLoggedIn = true;
+            return true;
+        } else {
+            isLoggedIn = false;
+            return false;
+        }
+    }
+}
