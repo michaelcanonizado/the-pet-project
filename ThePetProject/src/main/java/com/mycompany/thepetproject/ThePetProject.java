9c825515f8cc0a0ce288d56a44654bd8bbfcdcea
unknown <michaelxaviercanonizado@gmail.com>
2024-12-13 14:14:38 +0800

    feat(Home,BrowsePets): add temporary links to frames

--- ThePetProject/src/main/java/com/mycompany/thepetproject/ThePetProject.java
+++ ThePetProject/src/main/java/com/mycompany/thepetproject/ThePetProject.java
@@ -1,16 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.thepetproject;
 
 /**
  *
  * @author lilac
  */
+
+import com.mycompany.thepetproject.frames.Home;
+
 public class ThePetProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        java.awt.EventQueue.invokeLater(() -> {
+            Home home = new Home();
+            home.setVisible(true);
+        });
     }
 }
