3a9379915d913b8049f3872791b5cd435285475d
unknown <michaelxaviercanonizado@gmail.com>
2024-12-13 13:46:29 +0800

    chore: initialize NetBeans project

---
+++ ThePetProject/src/main/java/com/mycompany/thepetproject/ThePetProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thepetproject;
+
+/**
+ *
+ * @author lilac
+ */
+public class ThePetProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
