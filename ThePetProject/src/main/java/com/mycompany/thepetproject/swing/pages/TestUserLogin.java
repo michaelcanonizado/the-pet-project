c0b5c3f0a43efdeff6196f6f36471e1e0291681c
unknown <michaelxaviercanonizado@gmail.com>
2024-12-16 21:26:12 +0800

    chore: test dynamically loaded cards

--- ThePetProject/src/main/java/com/mycompany/thepetproject/swing/pages/TestUserLogin.java
+++ ThePetProject/src/main/java/com/mycompany/thepetproject/swing/pages/TestUserLogin.java
@@ -5,6 +5,7 @@
 package com.mycompany.thepetproject.swing.pages;
 import java.util.HashSet;
 import javax.swing.JTabbedPane;
+import com.mycompany.thepetproject.swing.components.TestCard;
 import java.util.Iterator;
 
 /**
@@ -20,6 +21,17 @@ public class TestUserLogin extends javax.swing.JPanel {
     public TestUserLogin(JTabbedPane pane) {
         initComponents();
         pageControllerTabbedPane = pane;
+        for (int i = 0; i < 4; i++) {
+            StringBuilder header = new StringBuilder("Header ");
+            StringBuilder description = new StringBuilder("Description ");
+            header.append(i+1);
+            description.append(i+1);
+            TestCard card = new TestCard(header.toString(), description.toString());
+            card.setVisible(true);
+            System.out.println("Creating card " + (i+1));
+            cardsContainer.add(card);
+        }
+        cardsContainer.repaint();
     }
     
 
@@ -34,6 +46,7 @@ public class TestUserLogin extends javax.swing.JPanel {
 
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        cardsContainer = new javax.swing.JPanel();
 
         setBackground(new java.awt.Color(0, 0, 204));
 
@@ -47,6 +60,8 @@ public class TestUserLogin extends javax.swing.JPanel {
             }
         });
 
+        cardsContainer.setBackground(new java.awt.Color(0, 102, 204));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -54,21 +69,26 @@ public class TestUserLogin extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(152, 152, 152)
+                        .addGap(165, 165, 165)
                         .addComponent(jLabel1))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(376, 376, 376)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(263, Short.MAX_VALUE))
+                        .addGap(392, 392, 392)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(38, 38, 38)
+                        .addComponent(cardsContainer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(250, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(271, 271, 271)
+                .addGap(20, 20, 20)
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(289, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(cardsContainer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(512, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -79,6 +99,7 @@ public class TestUserLogin extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel cardsContainer;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
