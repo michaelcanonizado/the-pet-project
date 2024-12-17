4baa06706266db49877461e6a0a263c105bb9086
unknown <michaelxaviercanonizado@gmail.com>
2024-12-18 03:02:45 +0800

    feat(AddPet): add fram

--- ThePetProject/src/main/java/com/mycompany/thepetproject/swing/pages/Dashboard.java
+++ ThePetProject/src/main/java/com/mycompany/thepetproject/swing/pages/Dashboard.java
@@ -38,6 +38,7 @@ public class Dashboard extends PageBlueprint {
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
+        addPetBtn = new javax.swing.JButton();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel7 = new javax.swing.JPanel();
         panelRound1 = new com.mycompany.thepetproject.swing.components.PanelRound();
@@ -132,6 +133,18 @@ public class Dashboard extends PageBlueprint {
             }
         });
 
+        addPetBtn.setText("Add pet");
+        addPetBtn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                addPetBtnMouseClicked(evt);
+            }
+        });
+        addPetBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addPetBtnActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -146,6 +159,10 @@ public class Dashboard extends PageBlueprint {
                         .addComponent(jButton4)
                         .addComponent(jButton3)))
                 .addContainerGap())
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(addPetBtn)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -154,7 +171,9 @@ public class Dashboard extends PageBlueprint {
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(35, 35, 35)
                 .addComponent(jButton1)
-                .addGap(63, 63, 63)
+                .addGap(18, 18, 18)
+                .addComponent(addPetBtn)
+                .addGap(22, 22, 22)
                 .addComponent(jButton2)
                 .addGap(65, 65, 65)
                 .addComponent(jButton3)
@@ -534,8 +553,18 @@ public class Dashboard extends PageBlueprint {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton4ActionPerformed
 
+    private void addPetBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addPetBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_addPetBtnActionPerformed
+
+    private void addPetBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_addPetBtnMouseClicked
+        AddPet addPet = new AddPet();
+        addPet.setVisible(true);
+    }//GEN-LAST:event_addPetBtnMouseClicked
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton addPetBtn;
     private java.awt.Canvas canvas1;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
