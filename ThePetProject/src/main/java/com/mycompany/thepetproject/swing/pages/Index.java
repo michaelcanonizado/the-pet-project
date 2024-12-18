2670ae3cb74579cfedc90b29b0ff7eafebd81481
deynklarys <144914287+deynklarys@users.noreply.github.com>
2024-12-18 10:40:06 +0800

    docs: remove unused license comments from multiple page classes

--- ThePetProject/src/main/java/com/mycompany/thepetproject/swing/pages/Index.java
+++ ThePetProject/src/main/java/com/mycompany/thepetproject/swing/pages/Index.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package com.mycompany.thepetproject.swing.pages;
 import com.mycompany.thepetproject.swing.pages.Home;
 import com.mycompany.thepetproject.swing.pages.AuthPanel;
